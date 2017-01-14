e5b536dfa4e4a554bfc7f138d089cebd7c3f7c85
Chris Steinmeyer <csteinmeyer@gmail.com>
2017-01-14 16:44:01 -0700

    add guardian

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,24 @@ config :logger, :console,
 config :phoenix, :generators,
   binary_id: true
 
+  #TODO: regen the secret key and move to an ENV var
+  #jwk = JOSE.JWK.generate_key({:ec, "P-521"}) |> JOSE.JWK.to_map |> elem(1)
+config :guardian, Guardian,
+  allowed_algos: ["HS512"],
+  verify_module: Guardian.JWT,
+  issuer: "ShoppingList",
+  ttl: { 30, :days },
+  allowed_drift: 2000,
+  verify_issuer: true,
+  secret_key: {MySecretKey, :fetch},
+  secret_key: %{
+    "crv" => "P-521",
+    "d" => "axDuTtGavPjnhlfnYAwkHa4qyfz2fdseppXEzmKpQyY0xd3bGpYLEF4ognDpRJm5IRaM31Id2NfEtDFw4iTbDSE",
+    "kty" => "EC",
+    "x" => "AL0H8OvP5NuboUoj8Pb3zpBcDyEJN907wMxrCy7H2062i3IRPF5NQ546jIJU3uQX5KN2QB_Cq6R_SUqyVZSNpIfC",
+    "y" => "ALdxLuo6oKLoQ-xLSkShv_TA0di97I9V92sg1MKFava5hKGST1EKiVQnZMrN3HO8LtLT78SNTgwJSQHAXIUaA-lV"
+  },
+  serializer: ShoppingList.GuardianSerializer
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
